618b9029ccf142fea36ba4fa3cac10651a9e7bac
ilhanbozcan <ilhanbozcann@gmail.com>
2019-10-31 15:17:20 +0300

    lab7 update

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.tictactoe;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -35,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
                 Button btn = (Button) row.getChildAt(j);
                 btn.setOnClickListener(new CellListener(i,j));
             }
+        }
+
+
+        if(savedInstanceState != null){
+            player1Turn = savedInstanceState.getBoolean("turn");
+            byte[] arr = savedInstanceState.getByteArray("board");
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++) {
+                    board[i][j] = arr[i*3 +j];
+                }
+            }
+
         }
     }
     class CellListener implements View.OnClickListener{
@@ -127,9 +142,6 @@ public class MainActivity extends AppCompatActivity {
             return playerValue;
         }
         
-        
-
-
 
 
         return -1;
@@ -143,6 +155,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putByteArray("board", toArray(board));
+        outState.putBoolean("turn", player1Turn);
+
+    }
+
+    private byte[] toArray(byte[][] matrix){
+        int row = matrix.length;
+        int col = matrix[0].length;
+        byte[] arr = new byte[row*col];
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++) {
+                arr[i*3+ j] = board[i][j];
+            }
+        }
+
+        return  arr;
+
+    }
+
     void setBoardEnabled(boolean enable){
         TableLayout table = findViewById(R.id.table);
 
@@ -151,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
             TableRow row =(TableRow) table.getChildAt(i);
             for(int j = 0; j<3 ; j++){
                 Button btn = (Button) row.getChildAt(j);
-                btn.setEnabled(true);
+                btn.setEnabled(enable);
 
 
             }
@@ -168,8 +203,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        TableLayout table = findViewById(R.id.table);
 
+
+        TableLayout table = findViewById(R.id.table);
         for(int i = 0; i<3; i++){
             TableRow row =(TableRow) table.getChildAt(i);
             for(int j = 0; j<3 ; j++){
@@ -181,5 +217,62 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    public boolean saveGame(MenuItem item){
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        long b = 0;
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++) {
+                b +=(long) (board[i][j] * Math.pow(10, i *3 +j));
+            }
+        }
+
+        editor.putLong("board", b);
+        editor.putBoolean("turn", player1Turn);
+        editor.commit();
+
+        return true;
+
+    }
+
+    public boolean loadGame(MenuItem item) {
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        player1Turn = preferences.getBoolean("turn", true);
+        long b = preferences.getLong("board", 0);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = (byte) ((b / Math.pow(10, i * 3 + j)) % 10);
+            }
+        }
+        //update the button labels
+
+        TableLayout table = findViewById(R.id.table);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button btn = (Button) row.getChildAt(j);
+                switch (board[i][j]) {
+                    case 0: btn.setText("");
+                        break;
+                    case 1: btn.setText(PLAYER_1_SYMBOL);
+                        break;
+                    case 2: btn.setText(PLAYER_2_SYMBOL);
+                        break;
+
+                }
+            }
+
+        }
+        return true;
+
+    }
+
+
+
+
 
 }
